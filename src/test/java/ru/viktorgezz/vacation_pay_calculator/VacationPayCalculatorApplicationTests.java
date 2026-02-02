ff51b2e9c051f5e89c8df64a4feb4bb97c706cbb
viktorgezz <viktorgezz@mail.ru>
2026-02-02 13:38:44 +0300

    init commit

---
+++ src/test/java/ru/viktorgezz/vacation_pay_calculator/VacationPayCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.viktorgezz.vacation_pay_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacationPayCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
